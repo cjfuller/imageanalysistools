13b09727edebe79c89fca29d7937efcb5c8c2bec
Colin Fuller <cjfuller@gmail.com>
2011-07-13 13:12:35 -0700

    Reimplemented ImageCoordinate to be compatible with any number of dimensions.
    Deprecated get/set methods that refer to coordinates by
    name in the method name.  Added convenience methods for working with
    standard XYZCT 5D coordinates.

--- src/edu/stanford/cfuller/imageanalysistools/image/ImageCoordinate.java
+++ src/edu/stanford/cfuller/imageanalysistools/image/ImageCoordinate.java
@@ -36,8 +36,12 @@
 
 package edu.stanford.cfuller.imageanalysistools.image;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Represents an integer-valued coordinate in a 5D image (X, Y, Z, color, time).
+ * Represents an integer-valued coordinate in an n-dimensional image.
  *
  * <p>
  * To avoid unnecessary object allocation when iterating over large images, this class provides basic object pooling capabilities.
@@ -63,24 +67,18 @@ public class ImageCoordinate implements java.io.Serializable{
 	static final int initialStaticCoordCount = 8;
 	
 	private static java.util.Deque<ImageCoordinate> availableStaticCoords;
-	private static java.util.Hashtable<ImageCoordinate, Boolean> inUse;
 	
-	private int x;
-	private int y;
-	private int z;
-	private int c;
-	private int t;
+	private HashMap<String, Integer> dimensionCoordinates;
+	private ArrayList<String> indexToStringMapping;
 			
 	static {
 
         synchronized(ImageCoordinate.class) {
 
             availableStaticCoords = new java.util.LinkedList<ImageCoordinate>();
-            inUse = new java.util.Hashtable<ImageCoordinate, Boolean>();
 
             for (int i = 0; i < initialStaticCoordCount; i++) {
                 ImageCoordinate c = new ImageCoordinate();
-                inUse.put(c, false);
                 availableStaticCoords.add(c);
 
             }
@@ -88,40 +86,111 @@ public class ImageCoordinate implements java.io.Serializable{
         }
 	}
 	
-	private ImageCoordinate(){}
+	private static ImageCoordinate getNextAvailableCoordinate() {
+		ImageCoordinate coord = null;
+		synchronized(ImageCoordinate.class) {
+			if (! availableStaticCoords.isEmpty()) {
+				coord = availableStaticCoords.removeFirst();
+			}
+		}
+		if (coord == null) {
+			coord = new ImageCoordinate();
+		} else {
+			coord.clear();
+		}
+		return coord;
+	}
+	
+	private ImageCoordinate(){
+		this.dimensionCoordinates = new HashMap<String, Integer>();
+		this.indexToStringMapping = new ArrayList<String>();
+	}
 
     /**
      * Gets the x-component of this ImageCoordinate.
      * @return  The x-component as an integer.
+     * @deprecated 	Use {@link #get(String, int)} or {@link #get(int)} instead.
      */
-	public int getX(){return this.x;}
+	public int getX(){return this.get("x");}
 
     /**
      * Gets the y-component of this ImageCoordinate.
      * @return  The y-component as an integer.
+     * @deprecated 	Use {@link #get(String, int)} or {@link #get(int)} instead.
      */
-	public int getY(){return this.y;}
+	public int getY(){return this.get("y");}
 
     /**
      * Gets the z-component of this ImageCoordinate.
      * @return  The z-component as an integer.
+     * @deprecated 	Use {@link #get(String, int)} or {@link #get(int)} instead.
      */
-	public int getZ(){return this.z;}
+	public int getZ(){return this.get("z");}
 
     /**
      * Gets the c-component of this ImageCoordinate.
      * @return  The c-component as an integer.
+     * @deprecated 	Use {@link #get(String, int)} or {@link #get(int)} instead.
      */
-	public int getC(){return this.c;}
+	public int getC(){return this.get("c");}
 
     /**
      * Gets the t-component of this ImageCoordinate.
      * @return  The t-component as an integer.
+     * @deprecated 	Use {@link #get(String, int)} or {@link #get(int)} instead.
      */
-	public int getT(){return this.t;}
+	public int getT(){return this.get("t");}
+	
+	/**
+	 * Gets the specified named coordinate component of the ImageCoordinate.
+	 * @param dimension		the name of the coordinate component to retrieve.
+	 * @return				the component in the named direction, or 0 if the component has not been specified.
+	 */
+	public int get(String dimension) {
+		if (this.dimensionCoordinates.containsKey(dimension)) {
+			return this.dimensionCoordinates.get(dimension);
+		}
+		return 0;
+	}
+	
+	/**
+	 * Gets the specified coordinate by index.
+	 * <p>
+	 * The order of coordinates will be the order in which they were set if done manually,
+	 * or the order from the coordinate this ImageCoordinate was cloned from, 
+	 * or otherwise ordered unpredictably (though consistent between calls), probably
+	 * according to hash key.
+	 * <p>
+	 * If the coordinate index is out of bounds, returns 0 instead of throwing an exception.
+	 * 
+	 * @param dimensionIndex	the index of the dimension whose value will be retrieved.
+	 * @return					the value of the specified dimension component, or 0 if the index was out of bounds.
+	 * 
+	 */
+	public int get(int dimensionIndex) {
+		if (dimensionIndex >= 0 && dimensionIndex < this.indexToStringMapping.size()) {
+			return this.get(this.indexToStringMapping.get(dimensionIndex));
+		}
+		return 0;
+	}
+	
+	/**
+	 * Sets the specified named coordinate component of the ImageCoordinate.
+	 * <p>
+	 * If the named component does not yet exist in the ImageCoordinate, it will be added.
+	 * 
+	 * @param dimension		the name of the coordinate component to set.
+	 * @param value			the value to which the named component will be set.
+	 */
+	public void set(String dimension, int value) {
+		if (! this.dimensionCoordinates.containsKey(dimension)) {
+			this.indexToStringMapping.add(dimension);
+		}
+		this.dimensionCoordinates.put(dimension, value);
+	}
 
     /**
-     * Factory method that creates an ImageCoordinate with the specified coordinates.
+     * Factory method that creates a 5D ImageCoordinate with the specified coordinates.
      *
      * This method will attempt to recycle an existing ImageCoordinate if any are available, or create a new one if none are available.
      *
@@ -135,37 +204,76 @@ public class ImageCoordinate implements java.io.Serializable{
      * @param t     The t-component of the ImageCoordinate to be created.
      * @return      An ImageCoordinate whose components have been set to the specified values.
      */
-	public static synchronized ImageCoordinate createCoord(int x, int y, int z, int c, int t) {
+	public static ImageCoordinate createCoordXYZCT(int x, int y, int z, int c, int t) {
 		
-		ImageCoordinate staticCoord = null;
-
-		if (! availableStaticCoords.isEmpty()) {
-			
-			staticCoord = availableStaticCoords.poll();
-			
-//			if (inUse.get(staticCoord)) {
-//				Logger.getLogger("edu.stanford.cfuller.imageanalysistools").warning("WARNING! Image coordinate was likely recycled twice: " + staticCoord.toString() + "  This can only result from a coding bug.");
-//				Logger.getLogger("edu.stanford.cfuller.imageanalysistools").warning("at:");
-//                try {
-//                    throw new IllegalStateException();
-//                } catch (IllegalStateException e) {
-//                    e.printStackTrace();
-//                }
-//                staticCoord = new ImageCoordinate();
-//			} 
-
-		} else {
-			staticCoord = new ImageCoordinate();
-		}
+		ImageCoordinate staticCoord = ImageCoordinate.getNextAvailableCoordinate();
 		
-		staticCoord.x = x;
-		staticCoord.y = y;
-		staticCoord.z = z;
-		staticCoord.c = c;
-		staticCoord.t = t;
-//		inUse.put(staticCoord, true);
+		staticCoord.set("x", x);
+		staticCoord.set("y", y);
+		staticCoord.set("z", z);
+		staticCoord.set("c", c);
+		staticCoord.set("t", t);
+
 		return staticCoord;
 		
+	}
+	
+	/**
+     * Factory method that creates a 5D ImageCoordinate with the specified coordinates.
+     *
+     * This method will attempt to recycle an existing ImageCoordinate if any are available, or create a new one if none are available.
+     *
+     * Users of this method should call {@link #recycle} on the ImageCoordinate returned by this method when finished with it in order to
+     * make the coordinate available for reuse on other calls of this method.
+     *
+     * @param x     The x-component of the ImageCoordinate to be created.
+     * @param y     The y-component of the ImageCoordinate to be created.
+     * @param z     The z-component of the ImageCoordinate to be created.
+     * @param c     The c-component of the ImageCoordinate to be created.
+     * @param t     The t-component of the ImageCoordinate to be created.
+     * @return      An ImageCoordinate whose components have been set to the specified values.
+     * @deprecated	use {@link #createCoordXYZCT(int, int, int, int, int)} instead.
+     */
+	public static ImageCoordinate createCoord(int x, int y, int z, int c, int t) {
+		
+		return ImageCoordinate.createCoordXYZCT(x, y, z, c, t);
+		
+	}
+	
+	/**
+     * Factory method that creates a zero-dimensional ImageCoordinate that can be manually extended.
+     *
+     * This method will attempt to recycle an existing ImageCoordinate if any are available, or create a new one if none are available.
+     *
+     * Users of this method should call {@link #recycle} on the ImageCoordinate returned by this method when finished with it in order to
+     * make the coordinate available for reuse on other calls of this method.
+     *
+     * @return      An ImageCoordinate with no specified dimensions.
+     */
+	public static ImageCoordinate createCoord() {
+		return ImageCoordinate.getNextAvailableCoordinate();
+	}
+	
+	/**
+     * Factory method that creates an n-dimensional ImageCoordinate whose dimensions and values
+     * are specified by the supplied Map, where n is the count of keys in the Map.
+     *
+     * This method will attempt to recycle an existing ImageCoordinate if any are available, or create a new one if none are available.
+     *
+     * Users of this method should call {@link #recycle} on the ImageCoordinate returned by this method when finished with it in order to
+     * make the coordinate available for reuse on other calls of this method.
+     *
+     * @param dimensionValues	A Map containing the name of each dimension as a String and each dimension's mapped value.
+     * @return      An ImageCoordinate with the specified dimensions and values.
+     */
+	public static ImageCoordinate createCoord(Map<String, Integer> dimensionValues) {
+		ImageCoordinate coord = ImageCoordinate.getNextAvailableCoordinate();
+		
+		for (String s : dimensionValues.keySet()) {
+			coord.set(s, dimensionValues.get(s));
+		}
+		
+		return coord;
 	}
 
 
@@ -179,9 +287,13 @@ public class ImageCoordinate implements java.io.Serializable{
      * @return          An ImageCoordinate whose components are set to the same values as the components of the supplied ImageCoordinate.
      */
     public static ImageCoordinate cloneCoord(ImageCoordinate toClone) {
-
-        return createCoord(toClone.getX(), toClone.getY(), toClone.getZ(), toClone.getC(), toClone.getT());
-
+    	
+    	ImageCoordinate newCoord = ImageCoordinate.getNextAvailableCoordinate();
+    	
+    	newCoord.setCoord(toClone);
+    	
+    	return newCoord;
+    	
     }
 
     /**
@@ -200,30 +312,42 @@ public class ImageCoordinate implements java.io.Serializable{
 
         synchronized(ImageCoordinate.class) {
 
-            /*if (availableStaticCoords.contains(this)) {
-                Logger.getLogger("edu.stanford.cfuller.imageanalysistools").warning("WARNING! Possible second recycle of: " + this.toString() + "  This can only result from a coding bug.");
-                Logger.getLogger("edu.stanford.cfuller.imageanalysistools").warning("at:");
-                try {
-                    throw new IllegalStateException();
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                }
-            } else {
-            */
             availableStaticCoords.add(this);
-            //}
-//          inUse.put(this, false);
+
         }
+	}
+	
+	/**
+	 * Gets the dimension of the ImageCoordinate: that is, the number of defined named dimensions
+	 * it contains.
+	 * 
+	 * @return		the dimension of the ImageCoordinate.
+	 */
+	public int getDimension() {
+		return this.indexToStringMapping.size();
 	}
 
     /**
      * Converts the ImageCoordinate to a string representation.
      * <p>
-     * The returned string will be "(x,y,z,c,t) = (a,b,c,d,e)", where a through e are placeholders for the actual coordinates of the ImageCoordinate.
+     * The returned string will be "(x,y,z,c,t) = (a,b,c,d,e)", where a through e 
+     * are placeholders for the actual coordinates of the ImageCoordinate, and x
+     * through t will be replaced by the defined named coordinates of the ImageCoordinate.
      * @return  The string representation of the ImageCoordinate.
      */
 	public String toString() {
-		String result = "(x,y,z,c,t) = (" + Integer.toString(this.x)+ ", "+ Integer.toString(this.y) + ", " + Integer.toString(this.z)+ ", " + Integer.toString(this.c) + ", " + Integer.toString(this.t) + ")";
+		if (this.getDimension() == 0) {
+			return "() = ()";
+		}
+		String keyString = "(" + this.indexToStringMapping.get(0);
+		String valueString = "(" + this.get(0);
+		for (int i = 1; i < this.getDimension(); i++) {
+			keyString+= "," + this.indexToStringMapping.get(i);
+			valueString+= "," + this.get(i);
+		}
+		keyString += ")";
+		valueString += ")";
+		String result = keyString + " = " + valueString;
 		return result;
 	}
 
@@ -236,12 +360,30 @@ public class ImageCoordinate implements java.io.Serializable{
      * @param c     The new c-component of the ImageCoordinate.
      * @param t     The new t-component of the ImageCoordinate.
      */
+	public void setCoordXYZCT(int x, int y, int z, int c, int t) {
+		this.set("x", x);
+		this.set("y", x);
+		this.set("z", x);
+		this.set("c", x);
+		this.set("t", x);
+	}
+	
+    /**
+     * Sets all the components of the ImageCoordinate to the specified values.
+     * 
+     * @param x     The new x-component of the ImageCoordinate.
+     * @param y     The new y-component of the ImageCoordinate.
+     * @param z     The new z-component of the ImageCoordinate.
+     * @param c     The new c-component of the ImageCoordinate.
+     * @param t     The new t-component of the ImageCoordinate.
+     * @deprecated	use {@link #setCoordXYZCT(int, int, int, int, int)} instead.
+     */
 	public void setCoord(int x, int y, int z, int c, int t) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.c = c;
-		this.t = t;
+		this.set("x", x);
+		this.set("y", x);
+		this.set("z", x);
+		this.set("c", x);
+		this.set("t", x);
 	}
 
     /**
@@ -249,47 +391,61 @@ public class ImageCoordinate implements java.io.Serializable{
      * @param other     The ImageCoordinate whose component values will be copied.
      */
     public void setCoord(ImageCoordinate other) {
-        this.setCoord(other.getX(), other.getY(), other.getZ(), other.getC(), other.getT());
+    	this.clear();
+    	for (String s : other.indexToStringMapping) {
+    		this.dimensionCoordinates.put(s, other.get(s));
+    		this.indexToStringMapping.add(s);
+    	}
+    }
+    
+    protected void clear() {
+    	this.dimensionCoordinates.clear();
+    	this.indexToStringMapping.clear();
     }
 
     /**
      * Sets the x-component of the ImageCoordinate to the specified value.
      * @param x The new x-component of the ImageCoordinate.
+     * @deprecated use {@link #set(String, int)} instead.
      */
 	public void setX(int x) {
-		this.x = x;
+		this.set("x", x);
 	}
 
     /**
      * Sets the y-component of the ImageCoordinate to the specified value.
      * @param y The new y-component of the ImageCoordinate.
+     * @deprecated use {@link #set(String, int)} instead.
      */
 	public void setY(int y) {
-		this.y = y;
+		this.set("y", y);
 	}
 
     /**
      * Sets the z-component of the ImageCoordinate to the specified value.
      * @param z The new z-component of the ImageCoordinate.
+     * @deprecated use {@link #set(String, int)} instead.
      */
 	public void setZ(int z) {
-		this.z = z;
+		this.set("z", z);
 	}
 
     /**
      * Sets the c-component of the ImageCoordinate to the specified value.
      * @param c The new c-component of the ImageCoordinate.
+     * @deprecated use {@link #set(String, int)} instead.
      */
 	public void setC(int c) {
-		this.c = c;
+		this.set("c", c);
 	}
 
     /**
      * Sets the t-component of the ImageCoordinate to the specified value.
      * @param t The new t-component of the ImageCoordinate.
+     * @deprecated use {@link #set(String, int)} instead.
      */
 	public void setT(int t) {
-		this.t = t;
+		this.set("t", t);
 	}
 	
 }
