3990b20d9d2e7f22cef874ccce98089e25436f04
Colin Fuller <cjfuller@gmail.com>
2012-04-16 15:27:25 -0700

    Added some javadoc and regenerated it.

--- src/edu/stanford/cfuller/imageanalysistools/filter/ImageSubtractionFilter.java
+++ src/edu/stanford/cfuller/imageanalysistools/filter/ImageSubtractionFilter.java
@@ -26,18 +26,42 @@ package edu.stanford.cfuller.imageanalysistools.filter;
 import edu.stanford.cfuller.imageanalysistools.image.Image;
 import edu.stanford.cfuller.imageanalysistools.image.ImageCoordinate;
 
+
+/**
+ * A Filter that subtracts one image from another.
+ * <p>
+ * A planar image can also be subtracted from a stack of images by calling the setSubtractPlanarImage method.
+ * <p>
+ * The reference Image is the image that will be subtracted.
+ *<p>
+ * The Image argument to the apply method will have the reference image subtracted from it in place.
+ *
+ * @author Colin J. Fuller
+ */
 public class ImageSubtractionFilter extends Filter {
 	
 	boolean subtractPlanarImage;
 	
+	/**
+	* Constructs a new ImageSubtractionFilter.  Defaults to not subtracting a planar image from each plane.
+	*/
 	public ImageSubtractionFilter() {
 		this.subtractPlanarImage = false;
 	}
 	
+	/**
+	* Sets whether the reference image is a planar image that should be subtracted from each plane of the image argument to the apply method.
+	* @param subPlanar whether the reference image should be subtracted from every plane.
+	* 
+	*/
 	public void setSubtractPlanarImage(boolean subPlanar) {
 		this.subtractPlanarImage = subPlanar;
 	}
 	
+	/**
+     * Appies the filter, subtracting the reference image from the argument to this method.
+     * @param im    The Image to be filtered; this will be filtered in place.
+     */
 	public void apply(Image im) {
 		
 		if (this.referenceImage == null) {
